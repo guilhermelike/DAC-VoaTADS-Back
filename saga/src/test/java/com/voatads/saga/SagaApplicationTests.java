f685ce35b93fa9ee2b12b1c24f8bbed4ade134ed
stromae <claudio.oliveira10.0p@gmail.com>
2024-11-21 09:22:03 -0300

    create saga project

---
+++ saga/src/test/java/com/voatads/saga/SagaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voatads.saga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
