00e1fb8bd44db702ee0158d28fe1686f149d95b6
Vinicius Ratzke Servelo <vinicius_servelo@hotmail.com>
2024-12-09 21:09:42 -0300

    SAGA Insert Employee Feita

---
+++ employee/src/main/java/com/voatads/employee/consumer/CreateEmployeeConsumer.java
@@ -0,0 +1,35 @@
+package com.voatads.employee.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.voatads.employee.dto.CreateEmployeeDTO;
+import com.voatads.employee.service.EmployeeService;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+
+@Service
+public class CreateEmployeeConsumer {
+
+    private static final Logger logger = LoggerFactory.getLogger(CreateEmployeeConsumer.class);
+
+    @Autowired
+    EmployeeService employeeService;
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @RabbitListener(queues = "create.employee.queue")
+    public void createEmployee(CreateEmployeeDTO messageReceived) {
+        logger.info("Recebido em employee consumer: {}", messageReceived);
+        try {
+            employeeService.createEmployee(messageReceived);
+            logger.info("Cliente criado com sucesso: {}", messageReceived);
+            rabbitTemplate.convertAndSend("saga.employee.success.queue", messageReceived);
+        } catch (Exception e) {
+            logger.error("Erro ao criar cliente: {}", messageReceived, e);
+        }
+    }
+}
---
+++ employee/src/main/java/com/voatads/employee/dto/CreateEmployeeDTO.java
@@ -0,0 +1,61 @@
+package com.voatads.employee.dto;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+public class CreateEmployeeDTO implements Serializable {
+    private UUID id;
+    private String cpf;
+    private String name;
+    private String email;
+    private String number;
+    private String password;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
