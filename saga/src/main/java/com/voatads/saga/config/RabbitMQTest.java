bcea449b912c0903d8b7abc4ee793b058cf89123
Guilherme <gfbatista1@gmail.com>
2024-12-12 15:37:40 -0300

    dockerfiles e compose corrigidos

--- saga/src/main/java/com/voatads/saga/config/RabbitMQTest.java
+++ saga/src/main/java/com/voatads/saga/config/RabbitMQTest.java
@@ -1,23 +1,23 @@
-package com.voatads.saga.config;
+// package com.voatads.saga.config;
 
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+// import org.springframework.amqp.rabbit.core.RabbitAdmin;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Component;
 
-import jakarta.annotation.PostConstruct;
+// import jakarta.annotation.PostConstruct;
 
 
-@Component
-public class RabbitMQTest {
+// @Component
+// public class RabbitMQTest {
 
-    @Autowired
-    private RabbitAdmin rabbitAdmin;
+//     @Autowired
+//     private RabbitAdmin rabbitAdmin;
 
-    @PostConstruct
-    public void testQueues() {
-        System.out.println("Verificando se as filas existem...");
-        System.out.println("Fila 'seat.flight.queue': " + rabbitAdmin.getQueueProperties("seat.flight.queue"));
-        System.out.println("Fila 'create.booking.queue': " + rabbitAdmin.getQueueProperties("create.booking.queue"));
-        System.out.println("Fila 'use.miles.customer.queue': " + rabbitAdmin.getQueueProperties("use.miles.customer.queue"));
-    }
-}
+//     @PostConstruct
+//     public void testQueues() {
+//         System.out.println("Verificando se as filas existem...");
+//         System.out.println("Fila 'seat.flight.queue': " + rabbitAdmin.getQueueProperties("seat.flight.queue"));
+//         System.out.println("Fila 'create.booking.queue': " + rabbitAdmin.getQueueProperties("create.booking.queue"));
+//         System.out.println("Fila 'use.miles.customer.queue': " + rabbitAdmin.getQueueProperties("use.miles.customer.queue"));
+//     }
+// }
