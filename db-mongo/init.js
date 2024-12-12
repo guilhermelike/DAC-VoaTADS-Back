0fd409a6594062b23c3651aa5f920a041d23f4e3
Guilherme <gfbatista1@gmail.com>
2024-12-12 09:59:58 -0300

    correções nos dockerfiles e docker compose

--- db-mongo/init.js
+++ db-mongo/init.js
@@ -1,17 +1,19 @@
 // Script de inicialização do MongoDB
-db = db.getSiblingDB("meubanco"); // Substitua "meubanco" pelo nome do banco desejado
+db = db.getSiblingDB("db_dac"); // Substitua "meubanco" pelo nome do banco desejado
 
 // Criar coleção "usuarios" e inserir um documento inicial
 db.usuarios.insertMany([
   {
     nome: "Admin",
     email: "admin@example.com",
-    senha: "senha123"
+    senha: "senha123",
+    type: 2
   },
   {
     nome: "User",
     email: "user@example.com",
-    senha: "senha456"
+    senha: "senha456",
+    type: 1
   }
 ]);
 
